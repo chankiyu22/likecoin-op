09eb1e40a4d4b5f59a0abfb0fbf651e14155e655
Ken Chan <chankiyu22@gmail.com>
2025-11-25 16:32:30 +0800

    operation: Wait for confirmation when fund operator

--- operation/src/fund-operator.ts
+++ operation/src/fund-operator.ts
@@ -1,8 +1,28 @@
-
-
 import { ethers } from "hardhat";
+import { TransactionReceipt } from "ethers";
+
+async function waitForConfirmations(
+  receipt: TransactionReceipt,
+  targetConfirmations: number
+) {
+  for (
+    let confirmations = await receipt.confirmations();
+    confirmations < targetConfirmations;
+    confirmations = await receipt.confirmations()
+  ) {
+    console.log(
+      `Awaiting confirmations... ${confirmations}/${targetConfirmations}`
+    );
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  }
+}
 
 async function fundOperator() {
+  let CONFIRMATIONS_TO_WAIT = 5;
+  if (process.env.CONFIRMATIONS_TO_WAIT) {
+    CONFIRMATIONS_TO_WAIT = parseInt(process.env.CONFIRMATIONS_TO_WAIT, 10);
+  }
+
   console.log("Funding operator");
   const funderKey = process.env.LOCALHOST_FUNDING_WALLET_PRIVATE_KEY;
   if (!funderKey) {
@@ -25,6 +45,12 @@ async function fundOperator() {
   console.log("Transaction hash", tx.hash);
   const receipt = await tx.wait();
   console.log("Transaction receipt", receipt);
+
+  if (receipt == null) {
+    throw new Error("Transaction receipt is null");
+  }
+
+  await waitForConfirmations(receipt, CONFIRMATIONS_TO_WAIT);
 
   const signerETH = await ethers.provider.getBalance(signer.address);
   console.log("Operator address balance", signerETH);
